704ded49182728a45f847adbed329b45046f95ed
Pablo Campos <pablocampos0512@gmail.com>
2026-01-23 19:51:57 -0400

    proyecto.cpp

--- Proyecto.cpp
+++ Proyecto.cpp
@@ -1,2 +1,11 @@
 #include<iostream>
 #include<string>
+
+
+using namespace std;
+
+struct Proyecto {
+    string nombre;
+    int duracion; // en meses
+    double presupuesto; // en miles de dólares
+};
